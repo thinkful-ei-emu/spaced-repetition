b4b1e848fa41484e369608bb6773f27f7ce29841
Martha Plantz <maplantz89@gmail.com>
2019-09-09 09:02:25 -0700

    ideas for stretch goal on day 2, waiting to pull down and test

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+//import TokenService from '../../services/token-service';
 
 class DashboardRoute extends Component {
+
+/**
+ * STRECT GOAL DAY 2
+  static defaultProps = { 
+    history: {
+      push: () => {},
+    },
+  }
+
+  onFailure = () => {
+    TokenService.clearAuthToken();
+    const {history} = this.props
+    history.push('/login')
+  }
+ */
+
+ 
+
   render() {
     return (
       <section>
