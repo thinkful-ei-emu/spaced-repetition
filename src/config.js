79b854da802b9ae3c42e9137723298965e4cf2ab
Colleen <colleenhiggins87@gmail.com>
2019-09-12 13:30:17 -0700

    testing config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.API_ENDPOINT ,
+  API_ENDPOINT: 'https://fast-coast-69616.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
-  API_KEY: process.env.REACT_APP_API_KEY
+  API_KEY: '8e04f0e5-f970-4c05-ae08-516e60149d9e'
 }
